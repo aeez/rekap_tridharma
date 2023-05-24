cda5c68731e162c6fa82a4ac4584b819b8a33e00
abdikhai <khairulabdi@mhs.unimed.ac.id>
2023-05-24 22:55:29 +0700

    perbaikan login admin

--- admin/login.php
+++ admin/login.php
@@ -60,29 +60,7 @@ if (isset($_POST['login'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>TRIDHARMA</title>
 
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
-      crossorigin="anonymous"
-    />
-
-    <!-- ICON -->
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"
-    />
-
-    <!-- FONT -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap"
-      rel="stylesheet"
-    />
-
-    <!-- CSS -->
-    <link rel="stylesheet" href="../app/assets/css/style.css" />
+    <?php include ('../app/layouts/layout_admin/link_admin.php'); ?>
 
   </head>
   <body>
@@ -121,7 +99,7 @@ if (isset($_POST['login'])) {
                       </div>
                     </div>
                     <div class="main-login-form mt-4">
-                      
+
                       <?php if (isset($error)) { ?>
                           <div class="alert alert-danger alert-dismissible" role="alert">
                               Gagal, mohon periksa lagi username dan password Anda!
